3b48d9ad865a9f74ccff5af76193cd9584195e12
irancss <irancssofficial@gmail.com>
2021-11-14 22:34:08 +0330

    Commit add Transport

---
+++ DesignPattern_Factory/Entities/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPattern_Factory.Interface;
+
+namespace DesignPattern_Factory.Entities
+{
+    public class Truck:ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Transport With Truck");
+        }
+    }
+}
